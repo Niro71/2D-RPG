b006db32439d39c47b17c0433a35693167abccc2
Tyler Harold <tyler@programmer.net>
2022-11-27 20:01:24 -0500

    update: movement & shooting

--- Assets/Scripts/Items/Equipable/Weapon.cs
+++ Assets/Scripts/Items/Equipable/Weapon.cs
@@ -13,7 +13,10 @@ public class Weapon : EquipableItem
   
   [Header("Bullet")]
   public GameObject bullet;
+  [Range(1, 20)]
   public int bulletSpeed;
+
+  private Transform firePoint; 
 
   public override void Use() {
     Debug.Log("Weapon equipped: " + itemName);
@@ -22,4 +25,19 @@ public class Weapon : EquipableItem
   public int calculateBaseDamage() {
     return Random.Range(minBaseDamage, maxBaseDamage);
   }
+
+  public void Fire(Player _player) {
+    GameObject bulletClone = Instantiate(bullet, _player.firePoint.position, Quaternion.identity);
+    Rigidbody2D bulletRb = bulletClone.GetComponent<Rigidbody2D>();
+
+    Vector3 direction = _player.p_mousePos - bulletClone.transform.position;
+    Vector3 rotation = bulletClone.transform.position - _player.p_mousePos;
+
+    bulletRb.velocity = new Vector2(direction.x, direction.y).normalized * (bulletSpeed * 5);
+
+    float rot = Mathf.Atan2(rotation.y, rotation.x) * Mathf.Rad2Deg;
+
+    // Add +90 to rot if the bullet is horizontal
+    bulletClone.transform.rotation = Quaternion.Euler(0, 0, rot);
+  } 
 }
--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -4,5 +4,69 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+  [Header("Player Stats")]
+  public int playerHealth;
+  public int maxPlayerHealth;
 
+  public int playerDamage;
+  public int playerSpeed;
+  public int playerDefense;
+
+  [Header("Player Equipment")]
+  public Weapon weapon;
+  public EquipableItem helmet;
+  public EquipableItem chestplate;
+  public EquipableItem leggings;
+  public EquipableItem boots;
+
+  [Header("Attached Objects")]
+  public Rigidbody2D rb;
+  public Camera cam;
+  public Transform firePoint;
+
+  [Header("Camera & Movement Info")]
+  public Vector2 p_Movement;
+  public Vector3 p_mousePos;
+
+  private void Awake() {
+    firePoint = this.transform.Find("FirePoint"); 
+    rb = GetComponent<Rigidbody2D>();
+  }
+
+  private void Update() {
+    // Updates p_Movement based on Input.GetAxisRaw()
+    UpdateMovementAxis();
+
+    // Updates mouse position based on ScreenToWorldPoint
+    UpdateMousePosition();
+
+    // If we're holding left click, fire our weapon
+    if (Input.GetMouseButtonDown(0)) {
+      weapon.Fire(this);
+    }
+  }
+
+  private void FixedUpdate() {
+    // Handle player movement
+    onPlayerMove();
+  }
+
+  private void UpdateMovementAxis() {
+    p_Movement.x = Input.GetAxisRaw("Horizontal");
+    p_Movement.y = Input.GetAxisRaw("Vertical");
+  }
+
+  private void UpdateMousePosition() {
+    p_mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
+    
+    Vector3 rotation = p_mousePos - firePoint.position;
+
+    float rotZ = Mathf.Atan2(rotation.y, rotation.x) * Mathf.Rad2Deg;
+
+    firePoint.rotation = Quaternion.Euler(0, 0, rotZ);
+  }
+
+  private void onPlayerMove() {
+    rb.MovePosition(rb.position + p_Movement * (playerSpeed * 10) * Time.fixedDeltaTime);
+  }
 }
